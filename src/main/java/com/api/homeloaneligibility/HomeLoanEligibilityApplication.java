d6f9f24f17750b0491d51550e772a7f73d5a17dd
Chiziane <eddybruno43@gmail.com>
2025-02-21 16:31:11 +0200

    First Commit

---
+++ src/main/java/com/api/homeloaneligibility/HomeLoanEligibilityApplication.java
@@ -0,0 +1,13 @@
+package com.api.homeloaneligibility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeLoanEligibilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeLoanEligibilityApplication.class, args);
+    }
+
+}
